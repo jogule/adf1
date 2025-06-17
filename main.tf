684ac4aa927a8938eba385fb2838933e9b12abd2
Jonathan <jogule@gmail.com>
2025-06-17 06:48:48 -0500

    Update .gitignore to exclude additional files and add main.tf and provider.tf for Azure resources configuration

--- main.tf
+++ main.tf
@@ -1,28 +1,36 @@
-terraform {
-    required_providers {
-        azurerm = {
-            source  = "hashicorp/azurerm"
-            version = "~> 4.0"
-        }
-    }
-    required_version = ">= 1.5"
-}
-
-provider "azurerm" {
-    subscription_id = var.subscription_id
-    client_id       = var.client_id
-    client_secret   = var.client_secret
-    tenant_id       = var.tenant_id
-
-    features {}
-}
-
 locals {
     resource_group_name = "adf1-rg"
     location            = "eastus"
+    sufix              = "jgl926"
 }
 
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "resource_group" {
   name     = local.resource_group_name
   location = local.location
+}
+
+resource "azurerm_storage_account" "storage_account" {
+  name                     = "st${local.sufix}"
+  resource_group_name      = azurerm_resource_group.resource_group.name
+  location                 = local.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "landing" {
+  name                  = "landing"
+  storage_account_id    = azurerm_storage_account.storage_account.id
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_container" "sampledata" {
+  name                  = "sampledata"
+  storage_account_id    = azurerm_storage_account.storage_account.id
+  container_access_type = "private"
+}
+
+resource "azurerm_data_factory" "data_factory" {
+  name                = "adf-${local.sufix}"
+  location            = local.location
+  resource_group_name = azurerm_resource_group.resource_group.name
 }
